b27cbb4bc04728e6ae6539c3ea8aa5a7cd9e10fd
Tommy Liao <tommyliao340@gmail.com>
2025-06-26 20:08:02 -0700

    Cleans up .gitignore
    Removes unnecesssary dependencies
    
    Cleans up scripts in package.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,21 +1,18 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  outDir: "dist",
+  entry: {
+    content: "src/index.ts", // becomes dist/public/content.js
+  },
+  outDir: "dist/public",
   format: ["esm"],
+  target: "esnext",
   minify: true,
   minifySyntax: true,
   minifyIdentifiers: true,
   minifyWhitespace: true,
   treeshake: true,
-  clean: true,
-  target: "esnext",
-  define: {
-    NODE_ENV: '"production"',
-  },
+  clean: false, // don't wipe manifest.json
   splitting: false,
-  sourcemap: false,
   dts: false,
-  watch: process.env.WATCH === "true", // allows optional watch mode
 });
